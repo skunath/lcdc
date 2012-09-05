8f4ab9fcd4fb0e488a28bb46de245f44f2c7630d
S Kunath <skunath@gmail.com>
2012-09-04 22:38:43 -0400

    prep for 3.2

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lcdc::Application.config.secret_token = '9d76983d23dad909ec13cabbdee9beedc59707bb63b2af43ba458fa28c833dee4f2db3c3ca9980194bd85cb73653c1032a6d0f2f46b8de25e21d1e6d61bd00d6'
