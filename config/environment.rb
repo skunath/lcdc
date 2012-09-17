d762a98cfedc5cc9f5ed4baa5166877ba2f035ad
S. Kunath <skunath@gmail.com>
2012-09-17 05:23:47 -0400

    fixes

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Lcdc::Application.initialize!
 
-#Lcdc::Application.config.LCDC_File_Location = "/projects/lcdc_support/data/"
 
-Lcdc::Application.config.LCDC_File_Location = "/projects/lcdc/temp_data/"
-Lcdc::Application.config.LCDC_New_File_Location = "/projects/lcdc/data-holder/"
+#Lcdc::Application.config.LCDC_File_Location = "/projects/lcdc/temp_data/"
+#Lcdc::Application.config.LCDC_New_File_Location = "/projects/lcdc/data-holder/"
+
+Lcdc::Application.config.LCDC_File_Location = "/var/www/html/lcdc-data/"
+Lcdc::Application.config.LCDC_New_File_Location = "/var/www/html/lcdc-data/data-holder/"
